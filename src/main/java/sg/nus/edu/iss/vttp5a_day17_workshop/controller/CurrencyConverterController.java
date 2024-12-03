b2ff5661ee942b7600817eb26baa0ad71be89bfd
remuslum <57741494+remuslum@users.noreply.github.com>
2024-12-03 16:16:56 +0800

    Added dependencies

---
+++ src/main/java/sg/nus/edu/iss/vttp5a_day17_workshop/controller/CurrencyConverterController.java
@@ -0,0 +1,10 @@
+package sg.nus.edu.iss.vttp5a_day17_workshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+public class CurrencyConverterController {
+    
+}
